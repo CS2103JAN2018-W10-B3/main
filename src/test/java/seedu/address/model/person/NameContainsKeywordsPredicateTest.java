57072e5000d20188e88245e4d02d18391323bc3f
tohcheryl <e0035693@u.nus.edu>
2018-03-08 18:40:07 +0800

    Update NameContainsKeywordsPredicateTest to reflect new attribute Price

--- src/test/java/seedu/address/model/person/NameContainsKeywordsPredicateTest.java
+++ src/test/java/seedu/address/model/person/NameContainsKeywordsPredicateTest.java
@@ -67,9 +67,10 @@ public class NameContainsKeywordsPredicateTest {
         predicate = new NameContainsKeywordsPredicate(Arrays.asList("Carol"));
         assertFalse(predicate.test(new PersonBuilder().withName("Alice Bob").build()));
 
-        // Keywords match phone, email and address, but does not match name
-        predicate = new NameContainsKeywordsPredicate(Arrays.asList("12345", "alice@email.com", "Main", "Street"));
+        // Keywords match phone, email, address and price, but does not match name
+        predicate = new NameContainsKeywordsPredicate(Arrays.asList("12345", "alice@email.com", "Main",
+                "Street", "76.50"));
         assertFalse(predicate.test(new PersonBuilder().withName("Alice").withPhone("12345")
-                .withEmail("alice@email.com").withAddress("Main Street").build()));
+                .withEmail("alice@email.com").withAddress("Main Street").withPrice("76.50").build()));
     }
 }
